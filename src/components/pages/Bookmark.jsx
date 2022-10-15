e173777b452082b168e43d0f4012e80cebe83eb6
array-in-a-matrix <tensor@arrayinamatrix.xyz>
2022-10-15 09:45:25 -0400

    created cards

--- src/components/pages/Bookmark.jsx
+++ src/components/pages/Bookmark.jsx
@@ -20,12 +20,15 @@ function Bookmark() {
   let recipeList = [];
   for (let i = 0; i < recipes.length; i++) {
     recipeList.push(
-      <div>
-        <img src={recipes[i].pic} width='33%'/> {/* //? image of recipe */}
-        <div>
-          <h2>{recipes[i].name}</h2> {/* //? name of recipe */}
-          <p>{recipes[i].discription}</p> {/* //? small discription of the recipe */}
-        </div>
+      <div className="d-flex card" key={i} style={{ margin: '5%', flexDirection: "row", alignItems: "center", padding: "1% 5%" }}>
+          <div className="col-sm" style={{ width: "30%", height: "100px",}}>
+            <img src={recipes[i].pic} style={{width: "100px", height: "100px",objectFit: "cover"}} /> {/* //? image of recipe */}
+          </div>
+              
+          <div  className="col-sm" style={{  width: "65%", marginLeft: '5%'}}>
+            <h2 style={{ fontSize: "125%" }}>{recipes[i].name}</h2> {/* //? name of recipe */}
+            <p style={{ fontSize: "80%" }}>{recipes[i].discription}</p> {/* //? small discription of the recipe */}
+          </div>
       </div>
     );
   }
