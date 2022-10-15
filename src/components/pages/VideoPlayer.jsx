3e7951b2bc2f1768a36bc89797f0300a3a9213aa
Ansel-Hong <anselhonggibby@gmail.com>
2022-10-15 00:53:22 -0400

    working fetch form firebase

--- src/components/pages/VideoPlayer.jsx
+++ src/components/pages/VideoPlayer.jsx
@@ -1,9 +1,51 @@
-function VideoPlayer() {
-    return <section>
-      <h1>Video Here</h1>
+import React from "react";
+import { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
 
-    </section>;
+function VideoPlayer() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedRecipe, setLoadedRecipe] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://htv7-96f00-default-rtdb.firebaseio.com/recipe.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const recipes = [];
+
+        for (const key in data) {
+          const recipe = {
+            id: key,
+            ...data[key],
+          };
+          recipes.push(recipe);
+        }
+
+        setIsLoading(false);
+        setLoadedRecipe(recipes);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
   }
-  
-  export default VideoPlayer;
-  
+
+  return (
+    <section>
+      <h1>Video Here</h1>
+      {/* <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" /> */}
+      <div>{ loadedRecipe.map( (recipe) => (
+        <ReactPlayer url={recipe.vid} /> 
+        ) ) }</div>
+      {/* <ReactPlayer> {loadedRecipe}</ReactPlayer> */}
+    </section>
+  );
+}
+
+export default VideoPlayer;
