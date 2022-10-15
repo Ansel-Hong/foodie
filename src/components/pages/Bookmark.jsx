36deac1935a93960e39a1f740bff7c0b60677edc
array-in-a-matrix <tensor@arrayinamatrix.xyz>
2022-10-15 18:33:18 -0400

    added button

--- src/components/pages/Bookmark.jsx
+++ src/components/pages/Bookmark.jsx
@@ -2,7 +2,6 @@ import RecipeList from "../store/recipe-context"
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 
-
 function Bookmark() {
   const recipeContext = useContext(RecipeList);
   const loadedRecipe = useContext(RecipeList).recipeList;
@@ -13,24 +12,23 @@ function Bookmark() {
 
   let recipeList = [];
   for (let i = 0; i < loadedRecipe.length; i++) {
-    console.log("bookmark", loadedRecipe[i].isBookmarked)
-    if(loadedRecipe[i].isBookmarked == true){
     recipeList.push(
-      <div className="d-flex card" key={i} style={{ margin: '2% 3%', flexDirection: "row", alignItems: "center", padding: "1% 5%" }} >
+      <div className="d-flex card" key={i} style={{ margin: '5%', flexDirection: "row", alignItems: "center", padding: "1% 5%" }} >
         <Link to="/" onClick={() => changeCurRecipe(i)}>
-          <div className="col-sm" style={{ width: "30%", height: "100px",}}>
-            <img src={loadedRecipe[i].pic} style={{width: "100px", height: "100px",objectFit: "cover", borderRadius: "15px"}} /> {/* //? image of recipe */}
+          <div className="col-sm" style={{ width: "30%", height: "100px", }}>
+            <img src={loadedRecipe[i].pic} style={{ width: "100px", height: "100px", objectFit: "cover" }} /> {/* //? image of recipe */}
           </div>
-          </Link>
-          <div  className="col-sm" style={{  width: "65%", marginLeft: '5%', paddingLeft: "5%"}}>
-            <h2 style={{ fontSize: "125%" }}>{loadedRecipe[i].name}</h2> {/* //? name of recipe */}
-            <p style={{ fontSize: "80%", fontFamily: 'Archivo', }} >{loadedRecipe[i].description}</p> {/* //? small description of the recipe */}
-          </div>
+        </Link>
+        <div className="col-sm" style={{ width: "65%", marginLeft: '5%', paddingLeft: "5%" }}>
+          <h2 style={{ fontSize: "125%" }}>{loadedRecipe[i].name}</h2> {/* //? name of recipe */}
+          <p style={{ fontSize: "80%", fontFamily: 'Archivo', }} >{loadedRecipe[i].description}</p> {/* //? small description of the recipe */}
+        </div>
+        <button onClick={() => recipeContext.unbookmarkRecipe(i)} className="btn btn-danger">Remove bookmark</button>
       </div>
-    );}
+    );
   }
   return <section>
-    <h1 style={{margin: "5% 0 5% 5%"}} >Bookmark recipes</h1>
+    <h1 style={{ margin: "5% 0 5% 5%" }} >Bookmark recipes</h1>
     {recipeList}
   </section>;
 }
