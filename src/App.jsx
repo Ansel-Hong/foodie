f66439005958fc2a062749dabe3e0e613c9b94f8
Aarya Prakash <aaryaprakash@Aaryas-MacBook-Air.local>
2022-10-15 01:07:44 -0400

    Receipe List

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import List from './List.jsx'
+import databaseInfo from './ExampleData.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [receipes, setReceipes] = useState([{id: databaseInfo.id, name: databaseInfo.name, urlPic: databaseInfo.linkToPic, urlVid: databaseInfo.linkToVid}])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <List receipes = {receipes}/>
+
+    </>
   )
 }
 
