cb21810378af22c5ae611a03a7fe2747ea981c25
array-in-a-matrix <tensor@arrayinamatrix.xyz>
2022-10-15 17:48:10 -0400

    format code

--- src/components/pages/Bookmark.jsx
+++ src/components/pages/Bookmark.jsx
@@ -16,19 +16,19 @@ function Bookmark() {
     recipeList.push(
       <div className="d-flex card" key={i} style={{ margin: '5%', flexDirection: "row", alignItems: "center", padding: "1% 5%" }} >
         <Link to="/" onClick={() => changeCurRecipe(i)}>
-          <div className="col-sm" style={{ width: "30%", height: "100px",}}>
-            <img src={loadedRecipe[i].pic} style={{width: "100px", height: "100px",objectFit: "cover"}} /> {/* //? image of recipe */}
+          <div className="col-sm" style={{ width: "30%", height: "100px", }}>
+            <img src={loadedRecipe[i].pic} style={{ width: "100px", height: "100px", objectFit: "cover" }} /> {/* //? image of recipe */}
           </div>
-          </Link>
-          <div  className="col-sm" style={{  width: "65%", marginLeft: '5%', paddingLeft: "5%"}}>
-            <h2 style={{ fontSize: "125%" }}>{loadedRecipe[i].name}</h2> {/* //? name of recipe */}
-            <p style={{ fontSize: "80%", fontFamily: 'Archivo', }} >{loadedRecipe[i].description}</p> {/* //? small description of the recipe */}
-          </div>
+        </Link>
+        <div className="col-sm" style={{ width: "65%", marginLeft: '5%', paddingLeft: "5%" }}>
+          <h2 style={{ fontSize: "125%" }}>{loadedRecipe[i].name}</h2> {/* //? name of recipe */}
+          <p style={{ fontSize: "80%", fontFamily: 'Archivo', }} >{loadedRecipe[i].description}</p> {/* //? small description of the recipe */}
+        </div>
       </div>
     );
   }
   return <section>
-    <h1 style={{margin: "5% 0 5% 5%"}} >Bookmark recipes</h1>
+    <h1 style={{ margin: "5% 0 5% 5%" }} >Bookmark recipes</h1>
     {recipeList}
   </section>;
 }
